fe3f76dc6e820693914d571154d8170927c0ea74
Julian Wareley <jwareley@hotmail.com>
2015-06-08 00:18:09 +0200

    First IoT Project

---
+++ IoTLogin/LoginService/DataObjects/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.WindowsAzure.Mobile.Service;
+
+namespace LoginService.DataObjects
+{
+    public class TodoItem : EntityData
+    {
+        public string Text { get; set; }
+
+        public bool Complete { get; set; }
+    }
+}
